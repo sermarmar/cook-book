717a46bb707d10c13ff80a2902e0d758ceb9b276
sermarmar <sermarmar1995@gmail.com>
2025-08-29 13:48:34 +0200

    Refactor add fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-4xl'>Hola mundo</h1>
+        <h1 className='text-4xl font-berlin-xbold'>Hola mundo</h1>
         <Button onClick={() => alert('Button clicked!')}>Pulsame</Button>
         <Button color='secondary' onClick={() => alert('Button clicked!')}>Pulsame</Button>
         <Button color='success' onClick={() => alert('Button clicked!')}>Pulsame</Button>
@@ -20,11 +20,24 @@ function App() {
         <Button color='danger' circle onClick={() => alert('Button clicked!')}>X</Button>
         <Input type='text' placeholder='Escribe' onChange={value => console.log(value)} />
         <ComboSelect list={['Option 1', 'Option 2', 'Option 3']} onSelect={value => console.log(value)} />
-        <Card>
-          <CardImage src="/foto.jpg" alt="Funciona" />
-          <CardTitle>Card Title</CardTitle>
-          <CardDecription>This is a description of the card.</CardDecription>
-        </Card>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <Card>
+            <CardImage src="/foto.jpg" alt="Funciona" />
+            <CardTitle>Card Title</CardTitle>
+            <CardDecription>This is a description of the card.</CardDecription>
+          </Card>
+          <Card>
+            <CardImage src="/foto.jpg" alt="Funciona" />
+            <CardTitle>Card Title</CardTitle>
+            <CardDecription>This is a description of the card.</CardDecription>
+          </Card>
+          <Card>
+            <CardImage src="/foto.jpg" alt="Funciona" />
+            <CardTitle>Card Title</CardTitle>
+            <CardDecription>This is a description of the card.</CardDecription>
+          </Card>
+        </div>
+        
       </div>
     </>
   )
